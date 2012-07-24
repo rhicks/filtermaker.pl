9f0cc994fcb5e300c0fa07fc6e3934fcb267f6b5
Richard Hicks <richard.hicks@gmail.com>
2012-07-23 17:59:44 -0700

    Initial commit

---
+++ filtermaker.pl
@@ -0,0 +1,111 @@
+#!/usr/bin/perl -w
+
+### Name:	 filtermaker.pl
+### Author:	 Richard J. Hicks (richard.hicks@gmail.com)
+### Date:	 2010-06-25
+### Description: This program downloads raw data from SPAMHAUS, CYMRU, OKEAN, 
+###              and builds Cisco friendly null routes.
+
+
+use strict;
+use Switch;
+use LWP::Simple;
+use Getopt::Std;
+use POSIX qw(strftime);
+
+my $currentDate = strftime "%Y-%m-%d", localtime;
+my $opt = 'abc';
+my %opt;
+getopts( $opt, \%opt );
+
+
+### VARIABLES ###
+my $spamhausURL  = "http://www.spamhaus.org/drop/drop.lasso";
+my $spamhausDesc = "SPAMHAUS-DROP-LIST_$currentDate";
+my $cymruURL     = "http://www.team-cymru.org/Services/Bogons/bogon-bn-nonagg.txt";
+my $cymruDesc    = "CYMRU-BOGON-LIST_$currentDate";
+my $okeanURL     = "http://www.okean.com/chinacidr.txt";
+my $okeanDesc    = "OKEAN-CHINA-LIST_$currentDate";
+#################
+
+&printHelp unless ($opt{a} or $opt{b} or $opt{c});
+if ($opt{a}) { printRoutes(get($spamhausURL), $spamhausDesc); }
+if ($opt{b}) { printRoutes(get($cymruURL), $cymruDesc); }
+if ($opt{c}) { printRoutes(get($okeanURL), $okeanDesc); }
+
+
+
+sub printHelp
+{
+        print "\n!!! USE THIS SCRIPT AT YOUR OWN RISK !!!\n";
+
+	print "\nThis Perl script creates null routes that can be installed on Cisco routers.\n";
+	print "When used in conjuction with uRPF, these filters provide an additional layer of SPAM, BOGON, and China filtering.\n";
+	print "Cisco uRPF Info: http://www.cisco.com/web/about/security/intelligence/unicast-rpf.html\n";
+
+        print "\nusage: $0 -a -b -c\n";
+	print "Options:\n";
+	print " -a  : Spamhaus DROP list data\n";
+	print " -b  : CYMRU Bogons list data\n";
+	print " -c  : OKEAN China Networks list\n";
+	exit 1;
+}
+
+
+
+
+sub printRoutes
+{
+	my @values = split(/\s/, $_[0]);
+	foreach my $val (@values)
+	{
+		if ($val =~ /^([\d]+)\.([\d]+)\.([\d]+)\.([\d]+)/)
+		{
+			my @newvalues = split(/\//, $val);
+			print "ip route " . $newvalues[0] . " " . computeMask($newvalues[1]) . " null0 name ". $_[1] . "\n";
+		}
+	}
+}
+
+
+
+sub computeMask
+{
+	switch ($_[0])
+	{
+		case 0  {return "0.0.0.0"}
+                case 1  {return "128.0.0.0"}
+                case 2  {return "192.0.0.0"}
+                case 3  {return "224.0.0.0"}
+                case 4  {return "240.0.0.0"}
+                case 5  {return "248.0.0.0"}
+                case 6  {return "252.0.0.0"}
+                case 7  {return "254.0.0.0"}
+                case 8  {return "255.0.0.0"}
+                case 9  {return "255.128.0.0"}
+                case 10 {return "255.192.0.0"}
+                case 11 {return "255.224.0.0"}
+                case 12 {return "255.240.0.0"}
+                case 13 {return "255.248.0.0"}
+                case 14 {return "255.252.0.0"}
+                case 15 {return "255.254.0.0"}
+                case 16 {return "255.255.0.0"}
+                case 17 {return "255.255.128.0"}
+                case 18 {return "255.255.192.0"}
+                case 19 {return "255.255.224.0"}
+                case 20 {return "255.255.240.0"}
+                case 21 {return "255.255.248.0"}
+                case 22 {return "255.255.252.0"}
+                case 23 {return "255.255.254.0"}
+                case 24 {return "255.255.255.0"}
+                case 25 {return "255.255.255.128"}
+                case 26 {return "255.255.255.192"}
+                case 27 {return "255.255.255.224"}
+                case 28 {return "255.255.255.240"}
+                case 29 {return "255.255.255.248"}
+                case 30 {return "255.255.255.252"}
+                case 31 {return "255.255.255.254"}
+                case 32 {return "255.255.255.255"}
+		else    {return ()}
+	}
+}
